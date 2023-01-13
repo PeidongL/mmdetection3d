a2485629ddd0cd876855ccf920e182225439b01c
rongbo.ma <rongbo.ma@plus.ai>
2023-01-13 02:28:07 +0000

    update plot_curv

--- tools/analysis_tools/plot_prcurv.py
+++ tools/analysis_tools/plot_prcurv.py
@@ -7,25 +7,6 @@ from typing import List
 
 
 schema = 'thres,pr,rec'.split(',')
-color='rgbcmyk'
-
-
-# def main(fds='', save_plot_path=''):
-#     print(fds)
-#     print('--------------',save_plot_path)
-#     fds = fds.split(',')
-    
-#     fns = filter(lambda x:(x.startswith('tpr') and not x.endswith('png')), os.listdir(fds[0]))
-    
-#     for i,fn in enumerate(fns):
-#         plt.figure(i)
-#         plt.title(fn)
-#         for j,fd in enumerate(fds):
-#             df1 = pd.read_csv(fd+"/%s"%fn, header=None, names=schema)
-#             label_name =  fd.split('/')[-5] +'_' + fd.split('/')[-1].split('checkpoint_')[1].split('.')[0]
-#             plt.plot(df1['rec'], df1['pr'], "*-%s"%color[j], label=label_name.replace('prefusion_',''))
-#         plt.legend(loc="lower left")
-#         plt.savefig(os.path.join(save_plot_path, '%s.png'%fn))
         
 def main(exps: List[str],indexs: List[int],distance: int,save_plot_path: str):
     tpr_files = []
@@ -52,6 +33,7 @@ def main(exps: List[str],indexs: List[int],distance: int,save_plot_path: str):
             tpr_file = os.path.join(exps[j], tpr_files[j][i])
             tpr = pd.read_csv(tpr_file, header=None, names=schema)
             label_name = exps[j].split('/')[-3] + '/' + exps[j].split('/')[-2] + '_e' + str(indexs[j])
+            # label_name = exps[j].split('/')[-3] + '/' + exps[j].split('/')[-2]
             plt.plot(tpr['rec'], tpr['pr'], "*-", label=label_name)
         plt.legend(loc="lower left")
         plt.savefig(os.path.join(save_plot_path, '%s.png'%plt_title))
@@ -74,24 +56,37 @@ if __name__ == '__main__':
     distance = 100
     exps = {
     "L3_data_models/bevdet_fusion/lidar/eval": 800, # lidar 800
-    "L3_data_models/bevdet_fusion/camera_fix_img_resize_bug_resume/eval": 60, 
+
+    # bevdet
+    "L3_data_models/bevdet_fusion/bevdet2.0_fusion_fix_resize_bug/eval": 800, # fusion
+    "L3_data_models/bevdet_fusion/camera_fea_45x80_depth_1x100/eval": 800, # camera 0-100m
+    # "L3_data_models/bevdet_fusion/camera_fix_img_resize_bug_resume/eval": 60, # camera 0-72m
+    
     # bev depth 
-    "L3_data_models/bevdepth_fusion/camera_resume/eval": 430,
+    # "L3_data_models/bevdepth_fusion/camera_resume/eval": 430, # camera 0-72， 没结果
     "L3_data_models/bevdepth_fusion/camera_retrain_0_100/eval": 800, # 800
     "L3_data_models/bevdepth_fusion/fusion_resume/eval": 560, # 560
-
-    "L3_data_models/bevdet_fusion/camera_fea_45x80_depth_1x100/eval": 800, # 800
+    
     "L3_data_models/pcdet_bev_fusion_height_pure/camera/eval": 160, #160
-    "L3_data_models/bevdet_fusion/bevdet2.0_fusion_fix_resize_bug/eval": 800, # 800
-    "L3_data_models/bevdet_fusion/camera_fix_img_resize_bug_resume/eval": 60, # 60
+    
+    }
+    
+    exps = {
+    "L3_data_models/bevdet_fusion/camera_fea_45x80_depth_1x100/eval": 800, # camera 0-100m
+        "L3_data_models/bevdet_fusion/camera_fix_resize_feature_bug/eval": [x*100 for x in range(8,9)],
+        "L3_data_models/bevdepth_fusion/bevdepth_camera_fix_resize_cam_fea/eval":  [x*100 for x in range(5,7)]
+        
     }
 
     
     
     save_plot_path = os.path.split(__file__)[0]
-    indexs = list(exps.values())
+    indexs = []
     exps_list = []
-    for exp in list(exps.keys()):
-        exps_list.append(os.path.join(root_dir, exp))
+    for name, val in exps.items():
+        index = val if isinstance(val, list) else [val]
+        for idx in index: 
+            exps_list.append(os.path.join(root_dir, name))
+            indexs.append(idx)
         
     main(exps_list, indexs, distance, save_plot_path)
